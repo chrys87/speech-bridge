f38d3f9440baecce431eee760eba9097c834ef35
chrys <chrys87@web.de>
2017-01-24 00:09:30 +0100

    sync

--- src/speechbridge/speechBridge.py
+++ src/speechbridge/speechBridge.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 from recognitionDriver import GoogleSpeechRecognition
+from core import environment
+from core import settingsManager
 
 class speechBridge():
     def __init__(self):
-        self.env = {}
-        self.env['running'] = True
-        self.env['recognitionDriver'] = GoogleSpeechRecognition.Driver()
+        self.env = environment.environment
+        self.env['runtime']['recognitionDriver'] = GoogleSpeechRecognition.Driver()
     def proceed(self):
-        while self.env['running']:
-            self.env['recognitionDriver'].listenToMicro()
-            text = self.env['recognitionDriver'].recognizeSpeech()
+        while self.env['general']['running']:
+            self.env['runtime']['recognitionDriver'].listenToMicro()
+            text = self.env['runtime']['recognitionDriver'].recognizeSpeech()
             if text.upper() == 'EXIT':
-                self.env['running'] = False
+                self.env['general']['running'] = False
+                print('stop')
             else:
                 print(text)
 
