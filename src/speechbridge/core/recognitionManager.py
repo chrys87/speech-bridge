6c231787c0308989311f5f50643374c19c13ca59
chrys87 <chrys87@web.de>
2017-01-24 09:44:26 +0100

    Create recognitionManager.py

---
+++ src/speechbridge/core/recognitionManager.py
@@ -0,0 +1,10 @@
+class recognitionManager():
+    def __init__(self):
+        pass
+    def initialize(self, environment):
+        self.env = environment
+    def shutdown(self):
+        pass
+    def recognize(self):
+        result = ''
+        return result
